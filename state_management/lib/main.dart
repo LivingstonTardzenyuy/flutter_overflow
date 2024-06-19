17ba65584ef594b3f01bb93efbc66ac078276af2
LivingstonTardzenyuy <LivingstonTardzenyuy>
2024-06-19 15:19:47 +0100

    working on watch, select consumer etc

--- state_management/lib/main.dart
+++ state_management/lib/main.dart
@@ -10,36 +10,66 @@ void main() {
   runApp(const MyApp());
 }
 
+
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
-      create: (BuildContext context) {
-        BreadCrumbProvider();
-      },
+      create: (BuildContext context) => BreadCrumbProvider(),  // Corrected this line
       child: MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(
-
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
         home: const HomePage(),
+        routes: {
+          '/new': (context) => const NewPage(),  // Ensure the route '/new' is defined and points to a valid widget
+        },
       ),
     );
   }
 }
+
+
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
+    // context.select<BreadCrumbProvider>((value) => null);
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        centerTitle: true,
+        title: const Text('Home Page', style: TextStyle(color: Colors.yellow, fontSize: 30),),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Text('hellow world'),
+          TextButton(
+              onPressed: (){
+                Navigator.of(context).pushNamed('/new');
+              },
+              child: const Text('Add new Crumb')
+          ),
+
+          const SizedBox(height: 50,),
+          TextButton(
+              onPressed: (){
+                final provider = context.read<BreadCrumbProvider>();
+                provider.reset();
+              },
+              child: const Text('Reset', style: TextStyle(color: Colors.red),)
+          ),
+        ],
+      )
     );
   }
 }
@@ -51,7 +81,8 @@ class BreadCrumbsWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Wrap(
+    
+    return Wrap(
       children: breadCrumbs.map((breadCrumb){
         return Text(
           breadCrumb.title,
@@ -60,6 +91,24 @@ class BreadCrumbsWidget extends StatelessWidget {
           ),
         );
       }).toList(),
+    );
+  }
+}
+
+
+
+class NewPage extends StatelessWidget {
+  const NewPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('New Page'),
+      ),
+      body: const Center(
+        child: Text('This is a new page'),
+      ),
     );
   }
 }
