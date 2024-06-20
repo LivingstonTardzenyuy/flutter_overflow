1dea5e9a34edee9a2bb80ec6a5d16721e18a2721
LivingstonTardzenyuy <LivingstonTardzenyuy>
2024-06-20 05:47:28 +0100

    statered another app under Provider

---
+++ detailsofprovider_course/lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const HomePage()
+    );
+  }
+}
+
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: const Text('Home Page'),
+      ),
+    );
+  }
+}
---
+++ detailsofprovider_course/lib/model/base_class.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:uuid/uuid.dart';
+@immutable
+class BaseObjects{
+  final String  id;
+  final String lastUpdate;
+  BaseObjects() : id = Uuid().v4(), lastUpdate = DateTime.now().toIso8601String();
+
+
+  @override bool operator == (covariant BaseObjects other) => id == other.id;
+
+  @override
+  int get hashCode => id.hashCode;
+}
+
+
+class ExpensiveObject extends BaseObjects{
+
+}
+
+
+class CheapObject extends BaseObjects{
+
+}
