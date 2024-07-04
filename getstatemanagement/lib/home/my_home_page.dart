1172954bf035b9f44a5daf00e6b3a5b56ac8ccb9
LivingstonTardzenyuy <LivingstonTardzenyuy>
2024-07-04 05:33:52 +0100

    wokrin

--- getstatemanagement/lib/home/my_home_page.dart
+++ getstatemanagement/lib/home/my_home_page.dart
@@ -1,50 +1,149 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+
 class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
 
-  void _showBottomSheet() {
-    Get.bottomSheet(
-      Container(
-        color: Colors.white,
-        child: Wrap(
-          children: <Widget>[
-            ListTile(
-              leading: Icon(Icons.music_note),
-              title: Text('Music'),
-              onTap: () => {},
-            ),
-            ListTile(
-              leading: Icon(Icons.videocam),
-              title: Text('Video'),
-              onTap: () => {},
-            ),
-          ],
-        ),
-      ),
-      backgroundColor: Colors.white,
-      isDismissible: true,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.vertical(top: Radius.circular(25.0)),
-      ),
-      enableDrag: true,
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.red,
-        title: Text('Hello'),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: _showBottomSheet,
-          child: Text('Show Bottom Sheet'),
-        ),
+    return Container(
+      color: Colors.white,
+      width: double.maxFinite,
+      height: double.maxFinite,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          GestureDetector(
+            onTap: () {},
+            child: Container(
+              margin: const EdgeInsets.all(20),
+              width: double.maxFinite,
+              height: 100,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: Color(0xFF80dad0),
+              ),
+              child: Center(
+                child: Text('Tap',
+                  style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.white
+                  ),
+                ),
+              ),
+            ),
+          ),
+
+          GestureDetector(
+            onTap: (){},
+            child: Container(
+              margin: const EdgeInsets.all(20),
+              width: double.maxFinite,
+              height: 100,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: Color(0xFF80dad0),
+              ),
+              child: Center(
+                child: Text('Tap',
+                  style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.white
+                  ),
+                ),
+              ),
+            ),
+          ),
+
+          Container(
+            margin: const EdgeInsets.all(20),
+            width: double.maxFinite,
+            height: 100,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              color: Color(0xFF80dad0),
+            ),
+            child: Center(
+              child: Text('Tap',
+                style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white
+                ),
+              ),
+            ),
+          ),
+
+          Container(
+            margin: const EdgeInsets.all(20),
+            width: double.maxFinite,
+            height: 100,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              color: Color(0xFF80dad0),
+            ),
+            child: Center(
+              child: Text('Tap',
+                style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white
+                ),
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
 }
+
+
+
+
+// class MyHomePage extends StatelessWidget {
+//   const MyHomePage({super.key});
+//
+//   void _showBottomSheet() {
+//     Get.bottomSheet(
+//       Container(
+//         color: Colors.white,
+//         child: Wrap(
+//           children: <Widget>[
+//             ListTile(
+//               leading: Icon(Icons.music_note),
+//               title: Text('Music'),
+//               onTap: () => {},
+//             ),
+//             ListTile(
+//               leading: Icon(Icons.videocam),
+//               title: Text('Video'),
+//               onTap: () => {},
+//             ),
+//           ],
+//         ),
+//       ),
+//       backgroundColor: Colors.white,
+//       isDismissible: true,
+//       shape: RoundedRectangleBorder(
+//         borderRadius: BorderRadius.vertical(top: Radius.circular(25.0)),
+//       ),
+//       enableDrag: true,
+//     );
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         backgroundColor: Colors.red,
+//         title: Text('Hello'),
+//       ),
+//       body: Center(
+//         child: ElevatedButton(
+//           onPressed: _showBottomSheet,
+//           child: Text('Show Bottom Sheet'),
+//         ),
+//       ),
+//     );
+//   }
+// }
